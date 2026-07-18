973fdc5a14281a8e8b93ba8e838088f043abe26a
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2026-07-18 09:46:07 +0700

    Write the logic to play a single round

--- main.js
+++ main.js
@@ -15,5 +15,53 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return prompt("Pick one: Rock, Paper, or Scissors")?.toLowerCase();
+  return prompt("Pick one: Rock, Paper, or Scissors");
 }
+
+function playRound(humanChoice, computerChoice) {
+  const humanPick = humanChoice.toLowerCase();
+
+  if (humanPick === "rock") {
+    if (computerChoice === "rock") {
+      console.log("Tie! Both players picked rock");
+    }
+    if (computerChoice === "paper") {
+      computerScore += 1;
+      console.log("Paper beats Rock. Computer Won!");
+    } else {
+      humanScore += 1;
+      console.log("Rock beats Scissors. You Won!");
+    }
+  }
+
+  if (humanPick === "paper") {
+    if (computerChoice === "paper") {
+      console.log("Tie! Both players picked Paper");
+    }
+    if (computerChoice === "scissors") {
+      computerScore += 1;
+      console.log("Scissors beats Paper. Computer Won!");
+    } else {
+      humanScore += 1;
+      console.log("Paper beats Rock. You Won!");
+    }
+  }
+
+  if (humanPick === "scissors") {
+    if (computerChoice === "scissors") {
+      console.log("Tie! Both players picked scissors");
+    }
+    if (computerChoice === "rock") {
+      computerScore += 1;
+      console.log("Rock beats Scissors. Computer Won!");
+    } else {
+      humanScore += 1;
+      console.log("Scissors beats Paper. You Won!");
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
